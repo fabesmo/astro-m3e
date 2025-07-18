2ae9ec317615643a20be9771c3dd26f15f4b6f5e
enotar.io <fabricioespinozamora@gmail.com>
2025-07-17 20:19:55 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+// INPUT
 export { default as TextField } from './components/inputs/TextField.astro';
 export { default as TextAreaField } from './components/inputs/TextAreaField.astro';
 export { default as SelectField } from './components/inputs/SelectField.astro';
+// COMMUNICATION
+export { default as Snackbar } from './components/communication/Snackbar.astro';
+// SYMBOLS
 export { default as Symbol } from './components/Symbol.astro';
