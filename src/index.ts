8a7a972512cfb04bb99d5312403c30c8218c6418
enotar.io <fabricioespinozamora@gmail.com>
2025-07-17 19:12:55 -0600

    Adding Buttons beta

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { default as TextField } from './components/inputs/TextField.astro';
+export { default as TextAreaField } from './components/inputs/TextAreaField.astro';
+export { default as SelectField } from './components/inputs/SelectField.astro';
+export { default as Symbol } from './components/Symbol.astro';
