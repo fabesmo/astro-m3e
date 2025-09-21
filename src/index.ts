1742aa6f5cf1aa0150613917d536de8a01865cad
enotar.io <fabricioespinozamora@gmail.com>
2025-09-21 13:19:20 -0600

    icon library update

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@
 export { default as TextField } from './components/inputs/TextField.astro';
 export { default as TextAreaField } from './components/inputs/TextAreaField.astro';
 export { default as SelectField } from './components/inputs/SelectField.astro';
+// ACTIONS
+export { default as Button } from './components/actions/Button.astro';
+export { default as Anchor } from './components/actions/Anchor.astro';
 // COMMUNICATION
 export { default as Snackbar } from './components/communication/Snackbar.astro';
 // SYMBOLS
